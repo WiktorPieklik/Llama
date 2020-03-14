7185e7d5612e5f6e0fdae03162a5b3326f4fc1e9
Maja <33836570+majabojarska@users.noreply.github.com>
2020-03-14 23:05:30 +0100

    Implement face landmark detection
    * Add 'Specs on Faces' facial recognition dataset.
    
    * Update Pipfile.lock, add dlib to deps
    
    * Face landmarks detection draft. Add required face models.
    
    * Implement FaceMask classes

--- src/face_landmark_detection.py
+++ src/face_landmark_detection.py
@@ -1,46 +1,32 @@
-import os
+import cv2
+import dlib
 
-from cv2 import cv2
+from face_mask import FaceMaskPoints
 
-from face_detector import FaceDetector
-from face_mask.points import FaceMaskPoints
-from frame_source.camera import CameraFrameSource
-from frame_source.source import ThreadedFrameSource
-from shape_predictor import ShapePredictor
-
-# from frame_source.video import VideoFrameSource
-
-print(os.getcwd())
-frame_source = CameraFrameSource(0)
-# frame_source = VideoFrameSource("data/external/pexels_video/1.mp4")
-# frame_source = ImageFrameSource(
-#     "data/external/sof/AboA_00148_m_33_i_nf_nc_hp_2016_2_e0_nl_o.jpg"
-# )
-if isinstance(frame_source, ThreadedFrameSource):
-    frame_source.start()
-
-detector = FaceDetector()
-predictor = ShapePredictor(
+cap = cv2.VideoCapture(0)
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(
     "../data/external/dlib_models/shape_predictor_68_face_landmarks.dat"
 )
 
-mask = FaceMaskPoints(point_radius=1, point_color=(255, 255, 0))
+mask = FaceMaskPoints(point_radius=2, point_color=(0, 255, 255))
 
-print("Starting processing loop")
-for frame in frame_source:
-
+while True:
+    _, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = detector.detect(gray)
+    faces = detector(gray)
     for face in faces:
-        landmarks = predictor.predict(gray, face)
+        # x1 = face.left()
+        # y1 = face.top()
+        # x2 = face.right()
+        # y2 = face.bottom()
+        # cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+        landmarks = predictor(gray, face)
         mask.apply(frame, landmarks)
 
     cv2.imshow("Frame", frame)
 
     key = cv2.waitKey(1)
     if key == 27:
-        if isinstance(frame_source, ThreadedFrameSource):
-            frame_source.join()
         break
-print("After processing loop")
