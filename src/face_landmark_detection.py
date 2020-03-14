6f9c7be371fabd55c9b6021fb84a09f7a9e254ee
Maja Bojarska <majabojarska98@gmail.com>
2020-03-14 22:59:45 +0100

    Implement FaceMask classes

--- src/face_landmark_detection.py
+++ src/face_landmark_detection.py
@@ -1,11 +1,15 @@
 import cv2
-import numpy as np
 import dlib
 
-cap = cv2.VideoCapture(0)
+from face_mask import FaceMaskPoints
 
+cap = cv2.VideoCapture(0)
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("../data/external/dlib_models/shape_predictor_68_face_landmarks.dat")
+predictor = dlib.shape_predictor(
+    "../data/external/dlib_models/shape_predictor_68_face_landmarks.dat"
+)
+
+mask = FaceMaskPoints(point_radius=2, point_color=(0, 255, 255))
 
 while True:
     _, frame = cap.read()
@@ -13,19 +17,13 @@ while True:
 
     faces = detector(gray)
     for face in faces:
-        x1 = face.left()
-        y1 = face.top()
-        x2 = face.right()
-        y2 = face.bottom()
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
-
+        # x1 = face.left()
+        # y1 = face.top()
+        # x2 = face.right()
+        # y2 = face.bottom()
+        # cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
         landmarks = predictor(gray, face)
-
-        for n in range(0, 68):
-            x = landmarks.part(n).x
-            y = landmarks.part(n).y
-            cv2.circle(frame, (x, y), 4, (255, 0, 0), -1)
-
+        mask.apply(frame, landmarks)
 
     cv2.imshow("Frame", frame)
 
