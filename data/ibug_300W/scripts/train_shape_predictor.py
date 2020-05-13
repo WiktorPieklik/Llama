79c6f445f3d8afebb4180d7fed7850d169c54c13
Maja Bojarska <majabojarska98@gmail.com>
2020-05-13 20:43:51 +0200

    Add --threads optional cmdline option, rename I/O options

--- data/ibug_300W/scripts/train_shape_predictor.py
+++ data/ibug_300W/scripts/train_shape_predictor.py
@@ -38,75 +38,75 @@ if __name__ == "__main__":
         "--threads",
         dest="threads",
         required=False,
-        help="number of threads to use for training",
+        help="number of threads to use for training. Defaults to CPU count.",
         type=int,
     )
     args = vars(ap.parse_args())
 
     # grab the default options for dlib's shape predictor
     print("[INFO] setting shape predictor options...")
-    options = dlib.shape_predictor_training_options()
+    training_options = dlib.shape_predictor_training_options()
 
     # define the depth of each regression tree -- there will be a total
     # of 2^tree_depth leaves in each tree; small values of tree_depth
     # will be *faster* but *less accurate* while larger values will
     # generate trees that are *deeper*, *more accurate*, but will run
     # *far slower* when making predictions
-    options.tree_depth = 4
+    training_options.tree_depth = 4
 
     # regularization parameter in the range [0, 1] that is used to help
     # our model generalize -- values closer to 1 will make our model fit
     # the training data better, but could cause overfitting; values closer
     # to 0 will help our model generalize but will require us to have
     # training data in the order of 1000s of data points
-    options.nu = 0.1
+    training_options.nu = 0.1
 
     # the number of cascades used to train the shape predictor -- this
     # parameter has a *dramtic* impact on both the *accuracy* and *output
     # size* of your model; the more cascades you have, the more accurate
     # your model can potentially be, but also the *larger* the output size
-    options.cascade_depth = 15
+    training_options.cascade_depth = 15
 
     # number of pixels used to generate features for the random trees at
     # each cascade -- larger pixel values will make your shape predictor
     # more accurate, but slower; use large values if speed is not a
     # problem, otherwise smaller values for resource constrained/embedded
     # devices
-    options.feature_pool_size = 400
+    training_options.feature_pool_size = 400
 
     # selects best features at each cascade when training -- the larger
     # this value is, the *longer* it will take to train but (potentially)
     # the more *accurate* your model will be
-    options.num_test_splits = 50
+    training_options.num_test_splits = 50
 
     # controls amount of "jitter" (i.e., data augmentation) when training
     # the shape predictor -- applies the supplied number of random
     # deformations, thereby performing regularization and increasing the
     # ability of our model to generalize
-    options.oversampling_amount = 5
+    training_options.oversampling_amount = 5
 
     # amount of translation jitter to apply -- the dlib docs recommend
     # values in the range [0, 0.5]
-    options.oversampling_translation_jitter = 0.1
+    training_options.oversampling_translation_jitter = 0.1
 
     # tell the dlib shape predictor to be verbose and print out status
     # messages our model trains
-    options.be_verbose = True
+    training_options.be_verbose = True
 
     # number of threads/CPU cores to be used when training -- we default
     # this value to the number of available cores on the system, but you
     # can supply an integer value here if you would like
     if args["threads"]:
-        target_thread_count = args.threads
+        target_thread_count = args["threads"]
     else:
         target_thread_count = multiprocessing.cpu_count()
 
-    options.num_threads = target_thread_count
+    training_options.num_threads = target_thread_count
 
     # log our training options to the terminal
     print("[INFO] shape predictor options:")
-    print(options)
+    print(training_options)
 
     # train the shape predictor
     print("[INFO] training shape predictor...")
-    train(args.input, args.output, options)
+    train(args["input"], args["output"], training_options)
