4160c9559b3140d8cc64f67b25d3275981403c29
Maja Bojarska <majabojarska98@gmail.com>
2020-05-13 22:13:06 +0200

    Move ibug 300W download script to the download dir

--- data/ibug_300W/download_dataset.sh
+++ data/ibug_300W/download_dataset.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+#
+# This script downloads and unpacks the ibug 300W dataset 
+# into the directory it's located in.
+#
+
 if [[ $(dirname "$0") != "." ]]; then
        echo "This script must be run from the directory it's located in."
        exit 1
@@ -7,5 +13,5 @@ fi
 wget http://dlib.net/files/data/ibug_300W_large_face_landmark_dataset.tar.gz
 tar -xvf ibug_300W_large_face_landmark_dataset.tar.gz
 rm ibug_300W_large_face_landmark_dataset.tar.gz
-mv ibug_300W_large_face_landmark_dataset/* ibug_300W
+mv ibug_300W_large_face_landmark_dataset/* .
 exit 0
