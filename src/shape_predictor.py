bee3170c615eeb12bdf7801424c3fe5da0aaacb7
Maja Bojarska <majabojarska98@gmail.com>
2020-05-17 14:25:49 +0200

    Add point remapping

--- src/shape_predictor.py
+++ src/shape_predictor.py
@@ -1,7 +1,10 @@
+from typing import Dict
 from pathlib import Path
 
 import dlib
 import numpy as np
+import os
+from config import PREDICTOR_LANDMARKS
 
 
 class ShapePredictor:
@@ -12,8 +15,9 @@ class ShapePredictor:
         model_path : str
             Path to the trained shape prediction model.
         """
-        abs_model_path = str(Path(model_path).resolve())
-        self._predictor: dlib.shape_predictor = dlib.shape_predictor(abs_model_path)
+        if not os.path.exists(model_path):
+            raise ValueError("Specified model file doesn't exist.")
+        self._predictor: dlib.shape_predictor = dlib.shape_predictor(model_path)
 
     def predict(
         self, im_gray: np.ndarray, face: dlib.rectangle
@@ -32,3 +36,15 @@ class ShapePredictor:
         dlib.full_object_detection
         """
         return self._predictor(im_gray, face)
+
+    def predict_remapped(
+        self, im_gray: np.ndarray, face: dlib.rectangle
+    ) -> Dict[int, dlib.point]:
+        return remap_points(self.predict(im_gray, face))
+
+
+def remap_points(detected_points: dlib.full_object_detection) -> dict:
+    remapped_points = {}
+    for i, part in enumerate(detected_points.parts()):
+        remapped_points[PREDICTOR_LANDMARKS[i]] = part
+    return remapped_points
