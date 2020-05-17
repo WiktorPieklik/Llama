8d634985ca3529fb0290783e999d599ebe3fa3b0
Maja Bojarska <majabojarska98@gmail.com>
2020-05-17 14:24:48 +0200

    Add predictor landmarks to config.

--- src/config.py
+++ src/config.py
@@ -4,7 +4,39 @@ SRC_DIR = Path(__file__).parent
 
 SHAPE_PREDICTOR_MODEL_PATH = str(
     (
-        SRC_DIR / "../data/external/dlib_models/shape_predictor_68_face_landmarks.dat"
+        SRC_DIR / "predictor_model.dat"
     ).resolve()
 )
 
+PREDICTOR_LANDMARKS = [
+    0,
+    2,
+    8,
+    14,
+    16,
+    17,
+    19,
+    21,
+    22,
+    24,
+    26,
+    27,
+    31,
+    33,
+    35,
+    36,
+    37,
+    39,
+    41,
+    42,
+    44,
+    45,
+    46,
+    48,
+    50,
+    52,
+    54,
+    57,
+    62,
+    66,
+]
