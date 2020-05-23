672e6f806ea505498e1da7de63009238e7ce2f3a
Maja Bojarska <majabojarska98@gmail.com>
2020-05-23 14:48:20 +0200

    Add point type conversion funcs to general utils.

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,41 @@
+import math
+import numpy as np
 import json
+import dlib
 
 
 def load_json(json_path: str) -> dict:
     with open(json_path, "r") as json_file:
         return json.load(json_file)
+
+
+def dlib_point_to_np_array(point: dlib.point):
+    """ Converts dlib.point instance to numpy array, as [x,y].
+
+    Parameters
+    ----------
+    point : dlib.point
+        The point to convert
+
+    Returns
+    -------
+    np.ndarray
+        Specified point converted to numpy array.
+    """
+    return np.array([point.x, point.y])
+
+
+def md_point_to_np_array(point: dict):
+    """ Converts metadata point to numpy array, as [x,y].
+
+    Parameters
+    ----------
+    point : dict
+        The metadata point to convert
+
+    Returns
+    -------
+    np.ndarray
+        Specified metadata point converted to numpy array.
+    """
+    return np.array([point["x"], point["y"]])
